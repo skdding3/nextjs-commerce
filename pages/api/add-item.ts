edf2e5507bbc244d246d3104f8de5290e30c5343
skdding3 <skdding3@gmail.com>
2023-01-01 22:17:19 +0900

    feat : add item result

--- pages/api/add-item.ts
+++ pages/api/add-item.ts
@@ -8,6 +8,8 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // add items
   const { name } = req.query
   res.status(200).json({ message: `Success ${name} added` })
+  // sucess jacket add
 }
