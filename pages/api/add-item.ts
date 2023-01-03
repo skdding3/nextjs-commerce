38cdd1c83ac4462570117969bc1940310616d068
skdding3 <skdding3@gmail.com>
2023-01-04 00:03:53 +0900

    feat : add item

--- pages/api/add-item.ts
+++ pages/api/add-item.ts
@@ -1,15 +1,52 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Client } from '@notionhq/client'
+
+const notion = new Client({
+  auth: 'secret_XQDRRLJyDc00qLbISPQ1VKrSujviXJz0kq5MyaRiQbX',
+})
+
+const databaseId = 'a7c908c8212c47f186178a4c38518191'
 
 type Data = {
   message: string
 }
 
-export default function handler(
+async function addItem(name: string) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: [
+          {
+            text: {
+              content: name,
+            },
+          },
+        ],
+      },
+    })
+  } catch (error) {
+    console.error(JSON.stringify(error))
+  }
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   // add items
   const { name } = req.query
-  res.status(200).json({ message: `Success ${name} added` })
+
+  if (name == null) {
+    return res.status(400).json({ message: 'No name' })
+  }
+
+  try {
+    await addItem(String(name))
+    res.status(200).json({ message: `Success ${name} added` })
+  } catch (error) {
+    res.status(400).json({ message: `Failed ${name} added` })
+  }
+
   // sucess jacket add
 }
